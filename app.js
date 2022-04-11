e1f5dfbd2ae4848453f4da7cbddc638ed95e9d44
Everson Magalhães <everson.m.cavalcante@gmail.com>
2022-04-11 15:50:32 -0300

    Add CORS access

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ const url = `mongodb+srv://${USER_NAME}:${PASSWORD}@${CLUSTER_NAME}/${DB_NAME}?r
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Acces-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 
 app.use("/api/users", usersRoutes);
